84076626eac7a0edf93275de914ffc1648537497
deni-rocha <denilsonrocha_@outlook.com>
2025-06-19 16:21:13 -0300

    entity cart-item

---
+++ src/cart-item/entities/cart-item.entity.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, BaseEntity } from 'typeorm';
+import { ShoppingCart } from '../../shopping-cart/entities/shopping-cart.entity'; // Importa ShoppingCart
+import { Product } from '../../product/entities/product.entity'; // Importa Product
+
+@Entity('cart_items')
+export class CartItem extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'int', nullable: false })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, name: 'unit_price', nullable: false })
+  unitPrice: number; // Preço do produto no momento em que foi adicionado ao carrinho
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', name: 'created_at', nullable: false })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP', name: 'updated_at', nullable: false })
+  updatedAt: Date;
+
+  // Relacionamento com ShoppingCart (Muitos itens de carrinho para um carrinho)
+  @ManyToOne(() => ShoppingCart, cart => cart.cartItems, {
+    nullable: false,
+    onDelete: 'CASCADE', // Se o carrinho for deletado, os itens também
+  })
+  @JoinColumn({ name: 'cart_id' })
+  cart: ShoppingCart;
+
+  // Relacionamento com Product (Um item de carrinho se refere a um produto)
+  @ManyToOne(() => Product, product => product.cartItems, {
+    nullable: false,
+    eager: true, // Carrega o produto junto com o item do carrinho
+  })
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+}
