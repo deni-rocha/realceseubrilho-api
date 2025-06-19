1d904454acd382eb74b87e2c0c87b498bf0a8aa6
deni-rocha <denilsonrocha_@outlook.com>
2025-06-19 00:56:18 -0300

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductCategoryModule } from './product-category/product-category.module';
+import { ProductModule } from './product/product.module';
+import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
+import { OrderModule } from './order/order.module';
+import { OrderItemModule } from './order-item/order-item.module';
+import { PaymentModule } from './payment/payment.module';
+import { DatabaseModule } from './database/role-sedeer.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'frevolink',
+      password: 'flpassword',
+      database: 'realcestore',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    DatabaseModule,
+    UsersModule,
+    ProductCategoryModule,
+    ProductModule,
+    ShoppingCartModule,
+    OrderModule,
+    OrderItemModule,
+    PaymentModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
