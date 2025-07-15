ab3dee862866c82d501264b067c775cf11a1d8d2
deni-rocha <denilsonrocha_@outlook.com>
2025-07-14 23:02:15 -0300

    endpoint raiz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRoot(): object {
+    return this.appService.getRoot();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getRoot(): object {
+    return {
+      name: 'Realce Seu Brilho API',
+      version: '1.0.0',
+      status: 'online',
+    };
   }
 }
